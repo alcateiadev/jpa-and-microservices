1c81998b08da85ec8b34c56153b3ecec898d29c8
Marcelo Vieira <marcelo.vieira@evoluum.com.br>
2020-12-30 01:35:02 -0300

    adicionado entidade para filhos da familia

---
+++ src/main/java/com/br/alcateiadev/jpa/jpaandmicroservice/entity/FamiliaFilhosEntity.java
@@ -0,0 +1,28 @@
+package com.br.alcateiadev.jpa.jpaandmicroservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "filhos_tabela")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class FamiliaFilhosEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "filhos_sequence")
+    private Long id;
+
+    @Column(name = "nome", length = 500)
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(name = "familia_id", nullable = false)
+    private FamiliaEntity familia;
+}
