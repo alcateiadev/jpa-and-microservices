095ed1f7f852532dd00c3cdabee114ece4c69657
Marcelo Vieira <marcelo.vieira@evoluum.com.br>
2020-12-30 01:31:08 -0300

    criado entidade familia com seu campos

--- src/main/java/com/br/alcateiadev/jpa/jpaandmicroservice/entity/FamiliaEntity.java
+++ src/main/java/com/br/alcateiadev/jpa/jpaandmicroservice/entity/FamiliaEntity.java
@@ -1,6 +1,24 @@
 package com.br.alcateiadev.jpa.jpaandmicroservice.entity;
 
-import javax.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "familia_tabela")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class FamiliaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "familia_sequence")
+    private Long id;
+
+    @Column(name = "nome", length = 500)
+    private String nome;
 }
